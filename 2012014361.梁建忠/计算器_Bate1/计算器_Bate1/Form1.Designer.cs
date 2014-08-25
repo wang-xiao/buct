003e744439bae27445975d2e3d0537f42cd17f34
JZLeung <375082677@qq.com>
2014-08-25 12:12:24 +0800

    Bata1
    只做了一个大概出来。bata1版本

---
+++ 2012014361.梁建忠/计算器_Bate1/计算器_Bate1/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace 计算器_Bate1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.C = new System.Windows.Forms.Button();
+            this.Back = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("微软雅黑", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(220, 34);
+            this.textBox1.TabIndex = 1;
+            // 
+            // C
+            // 
+            this.C.BackColor = System.Drawing.Color.Transparent;
+            this.C.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.C.Location = new System.Drawing.Point(12, 50);
+            this.C.Name = "C";
+            this.C.Size = new System.Drawing.Size(110, 26);
+            this.C.TabIndex = 3;
+            this.C.Text = "C";
+            this.C.UseVisualStyleBackColor = false;
+            this.C.Click += new System.EventHandler(this.C_Click);
+            // 
+            // Back
+            // 
+            this.Back.BackColor = System.Drawing.Color.Transparent;
+            this.Back.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Back.Location = new System.Drawing.Point(122, 50);
+            this.Back.Name = "Back";
+            this.Back.Size = new System.Drawing.Size(110, 26);
+            this.Back.TabIndex = 4;
+            this.Back.Text = "←";
+            this.Back.UseVisualStyleBackColor = false;
+            this.Back.Click += new System.EventHandler(this.Back_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(247, 306);
+            this.Controls.Add(this.Back);
+            this.Controls.Add(this.C);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.9D;
+            this.Text = "Calculator_Bata1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button C;
+        private System.Windows.Forms.Button Back;
+    }
+}
+
---
+++ 2012014361.梁建忠/计算器_Bate1/计算器_Bate1/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 计算器_Bate1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CreatNumBut();
+            this.ClientSize = new System.Drawing.Size(But_size * 4 + 24, But_size * 4 + 105);
+            //改变窗口大小
+            this.textBox1.Size = new System.Drawing.Size(But_size * 4, 34);
+            //改变textbox大小
+            this.C.Size = new System.Drawing.Size(But_size * 2, 26);
+            //改变C键大小
+            this.Back.Size = new System.Drawing.Size(But_size * 2, 26);
+            this.Back.Location = new System.Drawing.Point(12+But_size*2, 50);
+            //改变退格键大小及位置
+        }
+
+
+        int But_size = 60;//按钮大小.
+        int inputnum1 = 0, inputnum2 = 0;//标识正在输入num1/2
+        int clean = 0;//0标识未清零。清零了重新输入num1，未清零继续输入num2且进行计算
+        double num1 = 0, num2 = 0;//第一个数和第二个数
+        string strnum1 = "0", strnum2;//显示屏显示num1和num2.
+        int op;
+
+        private System.Windows.Forms.Button[] NumBut;
+        private System.Windows.Forms.Button[] OpeBut;
+        private System.Windows.Forms.Button Point;
+
+        void CreatNumBut()
+        {
+            NumBut = new System.Windows.Forms.Button[10];
+            OpeBut = new System.Windows.Forms.Button[5];
+
+            for (int i = 0; i < 10; i++)//添加数字键
+            {
+                NumBut[i] = new System.Windows.Forms.Button();
+                if (i == 0)
+                    NumBut[i].Location = new System.Drawing.Point(12 + 1 * But_size, 80 + 3 * But_size);
+                else
+                    NumBut[i].Location = new System.Drawing.Point(12 + (i - 1) % 3 * But_size, 80 + (i - 1) / 3 * But_size);
+                NumBut[i].Size = new System.Drawing.Size(But_size, But_size);
+                NumBut[i].Click += new System.EventHandler(NumButClick);
+                NumBut[i].Text = i.ToString();
+                NumBut[i].FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+                NumBut[i].BackColor = System.Drawing.Color.Transparent;
+                Controls.Add(NumBut[i]);
+            }
+            for (int i = 0; i < 5; i++)//添加运算符 及 = 号
+            {
+                OpeBut[i] = new System.Windows.Forms.Button();
+                OpeBut[i].Size = new System.Drawing.Size(But_size, But_size);
+                OpeBut[i].Location = new System.Drawing.Point(12 + 3 * But_size, 80 + i * But_size);
+                OpeBut[i].BackColor = System.Drawing.Color.Transparent;
+                OpeBut[i].FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+                switch(i)
+                {
+                    case 0:
+                        OpeBut[i].Text = "+";
+                        OpeBut[i].Click += new System.EventHandler(OpeButClick);
+                        break;
+                    case 1:
+                        OpeBut[i].Text = "-";
+                        OpeBut[i].Click += new System.EventHandler(OpeButClick);
+                        break;
+                    case 2:
+                        OpeBut[i].Text = "*";
+                        OpeBut[i].Click += new System.EventHandler(OpeButClick);
+                        break;
+                    case 3:
+                        OpeBut[i].Text = "/";
+                        OpeBut[i].Click += new System.EventHandler(OpeButClick);
+                        break;
+                    case 4:
+                        OpeBut[i].Text = "=";
+                        OpeBut[i].Location = new System.Drawing.Point(12 + 2 * But_size, 80 + 3 * But_size);
+                        OpeBut[i].Click += new System.EventHandler(SumClick);
+                        break;
+                }
+                Controls.Add(OpeBut[i]);
+            }
+
+            Point = new System.Windows.Forms.Button();
+            Point.Size = new System.Drawing.Size(But_size, But_size);
+            Point.Location = new System.Drawing.Point(12, 80 + 3 * But_size);
+            Point.Text = ".";
+            Point.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            Point.Click += new System.EventHandler(NumButClick);
+            Point.BackColor = System.Drawing.Color.Transparent;
+            Controls.Add(Point);
+        }//添加各种按钮
+
+        bool CheckPoint()//查看是否存在小数点
+        {
+            string temp;
+            if (inputnum1 == 0)
+                temp = strnum1;
+            else
+                temp = strnum2;
+            for (int i = 0; i < temp.Length; i++)
+                if (temp[i] == '.')
+                    return true;
+            return false;
+        }
+
+        private void NumButClick(object sender, EventArgs e)
+        {
+            string point = ".";
+            if (inputnum1 == 0)//输入num1
+            {
+                if (((Button)sender).Text == point)//输入小数点
+                {
+                    if (CheckPoint() == false)
+                        strnum1 += ((Button)sender).Text;
+                }
+                else if(strnum1 == "0")
+                {
+                    strnum1 = ((Button)sender).Text;
+                }
+                else
+                    strnum1 += ((Button)sender).Text;
+                textBox1.Text = strnum1;
+            }
+            else//输入num2
+            {
+                if (strnum2 == "0")
+                    strnum2 = ((Button)sender).Text;
+                else
+                {
+                    if (((Button)sender).Text == point)//输入小数点
+                    {
+                        if (CheckPoint() == false)
+                            strnum2 += ((Button)sender).Text;
+                    }
+                    else
+                        strnum2 += ((Button)sender).Text;
+                }
+                textBox1.Text = strnum2;
+                
+            }
+            
+        }//数字按钮响应
+        Button preBtu;
+        private void OpeButClick(object sender, EventArgs e)
+        {
+            if (inputnum1 == 0)
+            {
+                inputnum1 = 1;
+                preBtu = (Button)sender;
+            }
+            else
+                inputnum2++;
+            if (inputnum2 > 0)
+                //SumClick(null, null);
+                GetSum();
+            if (((Button)sender).Text != "=")
+            {
+                char temp = Convert.ToChar(((Button)sender).Text);
+                op = Convert.ToInt32(temp);
+            }
+            preBtu.ForeColor = ((Button)sender).ForeColor;
+            ((Button)sender).ForeColor = System.Drawing.Color.OrangeRed;
+            preBtu = (Button)sender;
+        }
+
+        private void SumClick(object sender, EventArgs e)
+        {
+            GetSum();
+            preBtu.ForeColor = ((Button)sender).ForeColor;
+            inputnum1 = 0;
+            inputnum2 = 0;
+        }
+
+        void GetSum()
+        {
+            num1 = Convert.ToDouble(strnum1);
+            num2 = Convert.ToDouble(strnum2);
+            //MessageBox.Show(num1.ToString() + (char)op + num2.ToString());
+            //MessageBox.Show(op.ToString());
+            switch(op)
+            {
+                case 43:
+                    num1 = num1 + num2;
+                    break;
+                case 45:
+                    num1 = num1 - num2;
+                    break;
+                case 42:
+                    num1 = num1 * num2;
+                    break;
+                case 47:
+                    num1 = num1 / num2;
+                    break;
+            }
+            num2 = 0;
+            strnum1 = num1.ToString();
+            strnum2 = num2.ToString();
+            textBox1.Text = strnum1;
+        }
+
+        private void Back_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("coming soon!");
+        }
+
+        private void C_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("coming soon!");
+        }
+    }
+}
